9a9112878628201cd2d91672386f12d85ca8295f
Christopher Korn <mail@chkorn.de>
2014-08-23 01:51:44 +0200

    revert

--- functions.php
+++ functions.php
@@ -3,17 +3,3 @@
  *  Custom functions
 /* ------------------------------------------------------------------------- */
 	
-if ( ! function_exists( 'alx_site_title' ) ) {
-	function alx_site_title() {
-		$link = '<a href="'.home_url('/').'" rel="home"><img src="'.ot_get_option('custom-logo').'" alt="'.get_bloginfo('name').'">'.get_bloginfo('name').'</a>';
-		
-		if ( is_front_page() || is_home() ) {
-			$sitename = '<h1 class="site-title">'.$link.'</h1>'."\n";
-		} else {
-			$sitename = '<p class="site-title">'.$link.'</p>'."\n";
-		}
-		
-		return $sitename;
-	}
-	
-}
