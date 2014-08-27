53c1a4d498dd5b996680b18eec2cef8d31216e86
Christopher Korn <mail@chkorn.de>
2014-08-27 23:57:37 +0200

    ...

--- functions.php
+++ functions.php
@@ -5,29 +5,29 @@
 
 // Override for Language
 if ( ! function_exists( 'alx_load' ) ) {
-	
+
 	function alx_load() {
 		// Load theme languages
-		load_theme_textdomain( 'hueman', get_stylesheet_directory().'/languages' );
-		
+		load_theme_textdomain( 'hueman', get_template_directory().'/languages' );
+		load_child_theme_textdomain('hueman', get_stylesheet_directory().'/languages');
+
 		// Load theme options and meta boxes
 		load_template( get_template_directory() . '/functions/theme-options.php' );
 		load_template( get_template_directory() . '/functions/meta-boxes.php' );
-		
+
 		// Load custom widgets
 		load_template( get_template_directory() . '/functions/widgets/alx-tabs.php' );
 		load_template( get_template_directory() . '/functions/widgets/alx-video.php' );
 		load_template( get_template_directory() . '/functions/widgets/alx-posts.php' );
-		
+
 		// Load custom shortcodes
 		load_template( get_template_directory() . '/functions/shortcodes.php' );
 
 		// Load dynamic styles
 		load_template( get_template_directory() . '/functions/dynamic-styles.php' );
-		
+
 		// Load TGM plugin activation
 		load_template( get_template_directory() . '/functions/class-tgm-plugin-activation.php' );
 	}
-	
+
 }
-add_action( 'after_setup_theme', 'alx_load' );	
